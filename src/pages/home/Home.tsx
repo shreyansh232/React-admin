f5286e6fb1c9e3fa89cedd84cbd78e05d08b2d67
Shreyansh Pandey <shreyansh487@gmail.com>
2024-06-22 12:23:27 +0530

    add piechart

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,7 @@
+
 import BarChartBox from "../../components/barChartBox/BarChartBox"
 import ChartBox from "../../components/chartBox/ChartBox"
+import PieChartBox from "../../components/pieChartBox/PieChartBox"
 import TopBox from "../../components/topBox/TopBox"
 import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data"
 import "./Home.scss"
@@ -12,7 +14,7 @@ const Home = () => {
       </div>
       <div className="box box2"><ChartBox  {...chartBoxUser}/></div>
       <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
-      <div className="box box4">4</div>
+      <div className="box box4"><PieChartBox/></div>
       <div className="box box5"><ChartBox {...chartBoxRevenue}/></div>
       <div className="box box6"><ChartBox {...chartBoxConversion}/></div>
       <div className="box box7">Box7</div>
