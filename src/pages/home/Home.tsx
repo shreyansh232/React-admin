18b78d65a22192831adc5f07b0d0079720e49ae7
Shreyansh Pandey <shreyansh487@gmail.com>
2024-06-22 12:29:03 +0530

    add bigChartBox

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 
 import BarChartBox from "../../components/barChartBox/BarChartBox"
+import BigChartBox from "../../components/bigChartBox/BigChartBox"
 import ChartBox from "../../components/chartBox/ChartBox"
 import PieChartBox from "../../components/pieChartBox/PieChartBox"
 import TopBox from "../../components/topBox/TopBox"
@@ -17,7 +18,7 @@ const Home = () => {
       <div className="box box4"><PieChartBox/></div>
       <div className="box box5"><ChartBox {...chartBoxRevenue}/></div>
       <div className="box box6"><ChartBox {...chartBoxConversion}/></div>
-      <div className="box box7">Box7</div>
+      <div className="box box7"><BigChartBox/></div>
       <div className="box box8"><BarChartBox {...barChartBoxVisit}/></div>
       <div className="box box9"><BarChartBox {...barChartBoxRevenue}/></div>
 
